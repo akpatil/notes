647587ffa540de7fc211fa8e7028dc5512b58902
akpatil <akshay78661@gmail.com>
2018-07-28 23:53:15 +0530

    Bash notes commit

---
+++ commit.sh
@@ -0,0 +1,10 @@
+echo "Start ==> commit"
+
+git add .
+echo "Files added"
+git commit -m "$1"
+echo "Files tagged for commit"
+echo "Pushing files to remote repository"
+git push -u origin master
+
+echo "End ==> commit"
